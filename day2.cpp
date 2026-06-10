24edfd0048f5b65c5e8c79b759dd0657dbbe78a9
Sofiya <sofiya@Sofiyas-MacBook-Air.local>
2026-06-10 15:14:29 +0530

    Add day2 DSA assignment

---
+++ day2.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> nextSmaller(vector<int>& nums)
+{
+    int n = nums.size();
+    vector<int> ans(n);
+    stack<int> st;
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        while(!st.empty() && st.top() >= nums[i])
+        {
+            st.pop();
+        }
+
+        if(st.empty())
+        {
+            ans[i] = -1;
+        }
+        else
+        {
+            ans[i] = st.top();
+        }
+
+        st.push(nums[i]);
+    }
+
+    return ans;
+}
